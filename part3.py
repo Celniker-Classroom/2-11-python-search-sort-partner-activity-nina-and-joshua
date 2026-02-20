50caa5d2740bae047c92adbb19806dbaa8c68dbf
iyern5136 <iyern5136@my.sduhsd.net>
2026-02-20 16:43:50 +0000

    hi

---
+++ part3.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+ranNums = [] #name your list and make sure it is empty!
+
+
+# Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
+for i in range(10): 
+    ranNums.append(randint(1, 50))
+
+print("The list is: " + str(ranNums))
+
+
+searchNumber = input("Enter a number between 1 and 50.")
